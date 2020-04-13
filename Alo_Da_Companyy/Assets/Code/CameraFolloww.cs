fe145aab1baf7a7c9cc1e1d3bb7e2ff7a272b047
George2G <51147744+George2G@users.noreply.github.com>
2020-04-13 18:41:40 +0300

    camera_outZoom

--- Alo_Da_Companyy/Assets/Code/CameraFolloww.cs
+++ Alo_Da_Companyy/Assets/Code/CameraFolloww.cs
@@ -6,7 +6,12 @@ public class CameraFolloww : MonoBehaviour
 {
     public Transform playerTransform;
     public float speed;
+    public Animator anim;
 
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatISGround;
 
 
     // Start is called before the first frame update
@@ -16,12 +21,27 @@ public class CameraFolloww : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatISGround);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (playerTransform != null)
         {
+
             transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);
+            if (isGrounded != true)
+            {
+                anim.SetBool("isIntheAir",true);
+            }
+            else
+            {
+                anim.SetBool("isIntheAir", false);
+
+            }
         }
 
     }
