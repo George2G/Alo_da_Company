de8bcc1adde9bb14e9c922ace6ae76bafb20f328
batfiowoof <bojidarlambov@abv.bg>
2020-04-27 13:01:07 +0300

    Revert "Revert "Parallax Effect Background""
    This reverts commit efc6c55d58e81f78cbd7c2a76ae1f5e443faaea9.

---
+++ Alo_Da_Companyy/Assets/Code/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float lenght, startpos;
+    public GameObject CameraFollow;
+    public float pEffect;
+    void Start()
+    {
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+
+    void Update()
+    {
+        float temp = (CameraFollow.transform.position.x * (1 - pEffect));
+        float dist = (CameraFollow.transform.position.x * pEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + lenght) startpos += lenght;
+        else if (temp < startpos - lenght) startpos -= lenght;
+    }
+}
